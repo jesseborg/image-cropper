db5562381d280a00fb11b2ea495670008f55502a
jesseborg <6882782+jesseborg@users.noreply.github.com>
2023-10-22 18:21:00 +1000

    add "Inter var" font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			sans: [
+				'Inter var, sans-serif',
+				{
+					fontFeatureSettings: '"cv11", "ss01"',
+					fontVariationSettings: '"opsz" 32'
+				}
+			]
+		}
 	},
-	plugins: [],
+	plugins: []
 };
