829ba2d2acab1edf313c3b6c5acada79e9e99179
jesseborg <6882782+jesseborg@users.noreply.github.com>
2023-10-24 22:25:22 +1000

    change input styling

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -6,7 +6,7 @@ export function Input({ ...props }: InputProps) {
 	return (
 		<input
 			type="text"
-			className="flex-1 rounded-lg border border-neutral-300 px-4 py-2 text-xs text-neutral-800 outline-none ring-blue-500 ring-offset-2 placeholder:text-neutral-400/80 focus-visible:ring-2"
+			className="flex-1 rounded-lg border border-neutral-300 px-4 py-1 text-xs font-medium text-black outline-none ring-blue-500 ring-offset-2 placeholder:text-neutral-400/80 focus-visible:ring-2"
 			{...props}
 		/>
 	);
