56e1a09f45ec2d5c060029a43047dfc20738b4b9
jesseborg <6882782+jesseborg@users.noreply.github.com>
2023-10-27 17:19:18 +1000

    add error state to input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,13 +1,34 @@
+import { cva } from 'class-variance-authority';
+import clsx from 'clsx';
 import { InputHTMLAttributes } from 'react';
 
-type InputProps = object & InputHTMLAttributes<HTMLInputElement>;
+const input = cva(
+	[
+		'rounded-lg flex-grow border border-neutral-300 px-4 py-1 text-xs font-medium text-black outline-none placeholder:text-neutral-400/80',
+		'focus-visible:ring-2 ring-offset-2 ring-blue-500'
+	],
+	{
+		variants: {
+			error: {
+				true: 'border-red-500 border-2'
+			}
+		}
+	}
+);
 
-export function Input({ ...props }: InputProps) {
+type InputProps = {
+	error?: string;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+export function Input({ error, className, ...props }: InputProps) {
+	const hasError = !!(error && error?.trim().length > 0);
+
 	return (
-		<input
-			type="text"
-			className="flex-1 rounded-lg border border-neutral-300 px-4 py-1 text-xs font-medium text-black outline-none ring-blue-500 ring-offset-2 placeholder:text-neutral-400/80 focus-visible:ring-2"
-			{...props}
-		/>
+		<span className="relative flex flex-grow">
+			<input type="text" className={clsx(className, input({ error: hasError }))} {...props} />
+			{hasError && (
+				<p className="absolute top-full pl-1 text-[11px] font-bold text-red-500">{error}</p>
+			)}
+		</span>
 	);
 }
