56e05975f7711ca1a6c9e6317038d7401d9d60cd
jesseborg <6882782+jesseborg@users.noreply.github.com>
2023-10-29 21:34:13 +1000

    move 'ImageUpload' to its own file

---
+++ src/components/image-upload.tsx
@@ -0,0 +1,125 @@
+import clsx from 'clsx';
+import { BanIcon, ImagePlus } from 'lucide-react';
+import { useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { z } from 'zod';
+import { Button } from './button';
+import { Input } from './input';
+
+const ACCEPTED_FILE_TYPES = ['png', 'jpeg', 'jpg', 'bmp'];
+
+const imageURLSchema = z
+	.string()
+	.min(1, ' ')
+	.url("Please check that your link start with 'http://' or 'https://'");
+
+type ImageUploadProps = {
+	onImageLoad?: (url: string) => void;
+};
+
+export function ImageUpload({ onImageLoad }: ImageUploadProps) {
+	const [imageURL, setImageURL] = useState('');
+
+	const validationResult = imageURLSchema.safeParse(imageURL);
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	const validationErrors = validationResult.error?.issues;
+
+	const { isDragReject, getRootProps, getInputProps, isDragActive, open } = useDropzone({
+		multiple: false,
+		noClick: true,
+		noKeyboard: true,
+		accept: Object.fromEntries(ACCEPTED_FILE_TYPES.map((ext) => [`image/${ext}`, []])),
+		onDrop: async (files) => {
+			if (!files.length) {
+				return;
+			}
+
+			const blob = URL.createObjectURL(files[0]);
+			onImageLoad?.(blob);
+		}
+	});
+
+	async function handleGetImageByURL(url: string) {
+		const response = await fetch(url);
+
+		if (!response.ok) {
+			return;
+		}
+
+		const contentType = response.headers.get('content-type');
+
+		if (!contentType || !contentType.startsWith('image/')) {
+			return;
+		}
+
+		// 'image/<type>'
+		if (!ACCEPTED_FILE_TYPES.includes(contentType.split('/')[1])) {
+			return;
+		}
+
+		const blob = URL.createObjectURL(await response.blob());
+		onImageLoad?.(blob);
+	}
+
+	return (
+		<div
+			className={clsx(
+				'flex flex-col gap-4 rounded-lg border-2 border-dashed border-neutral-200 bg-neutral-100 p-4',
+				{
+					'!border-blue-500 !bg-blue-50': isDragActive,
+					'!border-red-500 !bg-red-50': isDragReject
+				}
+			)}
+			{...getRootProps()}
+		>
+			<Button
+				intent="blank"
+				className={clsx(
+					'flex w-[500px] flex-grow select-none flex-col items-center justify-center gap-2 rounded-lg stroke-black !tracking-normal transition-none hover:bg-neutral-200/60',
+					{ 'stroke-blue-500 text-blue-500': isDragActive },
+					{ 'stroke-red-500 text-red-500': isDragReject }
+				)}
+				onClick={open}
+			>
+				<input {...getInputProps()} />
+				{!isDragReject && <ImagePlus className="h-12 w-12 stroke-inherit" />}
+				{isDragReject && <BanIcon className="h-12 w-12 stroke-inherit" />}
+				<div className="space-y-1 text-center">
+					<p className="text-sm font-medium">
+						{isDragActive
+							? isDragReject
+								? 'Wrong file type!'
+								: 'Drop image here'
+							: 'Choose files or drag and drop'}
+					</p>
+					{!isDragActive && (
+						<p className="text-xs font-medium tracking-wide text-neutral-400">
+							accepts {ACCEPTED_FILE_TYPES.join(', ')}
+						</p>
+					)}
+				</div>
+			</Button>
+			{!isDragActive && (
+				<>
+					<hr className="h-[2px] bg-neutral-200" />
+					<div className="flex gap-2">
+						<Input
+							error={validationErrors?.[0].message}
+							placeholder="Paste image link..."
+							value={imageURL}
+							onChange={(event) => setImageURL(event.currentTarget.value)}
+						/>
+						<Button
+							disabled={!!validationErrors?.length}
+							intent="primary"
+							onClick={() => handleGetImageByURL(imageURL)}
+						>
+							Search
+						</Button>
+					</div>
+				</>
+			)}
+		</div>
+	);
+}
