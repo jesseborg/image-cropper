e1c3fc2c15b85a700ffa193011db8837c95bbec5
jesseborg <6882782+jesseborg@users.noreply.github.com>
2023-11-06 00:36:56 +1000

    fix: CropTool bounds calculation

--- src/components/image-editor.tsx
+++ src/components/image-editor.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-
 import { animated, useSpring } from '@react-spring/web';
 import { useDrag } from '@use-gesture/react';
 import clsx from 'clsx';
@@ -10,6 +9,8 @@ import {
 	useControls,
 	useTransformEffect
 } from 'react-zoom-pan-pinch';
+import { clamp } from '../utils/clamp';
+import { getRelativeBounds } from '../utils/get-relative-bounds';
 import { Button } from './button';
 
 type ImageEditorProps = {
@@ -40,15 +41,19 @@ export function ImageEditor({ src, onCancel, onConfirm }: ImageEditorProps) {
 					centerZoomedOut
 					limitToBounds={false}
 					doubleClick={{ disabled: true }}
-					panning={{ velocityDisabled: true, allowRightClickPan: false }}
+					panning={{
+						velocityDisabled: true,
+						allowRightClickPan: false,
+						excluded: ['panning-none']
+					}}
 				>
-					<CropTool />
 					<TransformComponent
 						wrapperClass={clsx('relative rounded-lg border border-neutral-200 shadow-lg', {
 							'min-h-[350px] min-w-[350px]': size.width < 350 || size.height < 350
 						})}
-						contentClass="flex z-0 h-full flex-1 overflow-hidden w-full"
+						contentClass="flex z-0 h-full flex-1 w-full"
 					>
+						<CropTool />
 						<div className="flex max-h-full w-full items-center justify-center">
 							<img className="max-h-full" src={src} onLoad={handleImageLoad} />
 						</div>
@@ -82,29 +87,53 @@ function CropTool() {
 	const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }));
 	const bind = useDrag(
 		({ offset: [dx, dy] }) => {
-			api.start({ x: dx, y: dy, immediate: true });
+			api.start({ x: dx / scale, y: dy / scale, immediate: true });
 		},
 		{
-			bounds: { current: instance.wrapperComponent },
-			from: () => [x.get(), y.get()]
+			bounds: () => {
+				const containerElement = instance.contentComponent;
+				const imageElement = containerElement?.querySelector('img');
+				const containerRect = containerElement?.getBoundingClientRect();
+
+				const cropRect = cropRef.current?.getBoundingClientRect();
+
+				if (!imageElement || !containerElement || !containerRect || !cropRect) {
+					return {};
+				}
+
+				const relativeImageRect = getRelativeBounds(containerElement, imageElement);
+
+				return {
+					top: Math.max(0, relativeImageRect.top),
+					left: Math.max(0, relativeImageRect.left),
+					right: Math.min(containerRect.width, relativeImageRect.right) - cropRect.width,
+					bottom: Math.min(containerRect.height, relativeImageRect.bottom) - cropRect.height
+				};
+			},
+			from: () => [x.get() * scale, y.get() * scale]
 		}
 	);
 
 	useEffect(() => {
 		function keepCropToolInBounds() {
-			if (!cropRef.current || !instance.wrapperComponent) {
+			const containerElement = instance.contentComponent;
+			const imageElement = containerElement?.querySelector('img');
+
+			const cropRect = cropRef.current?.getBoundingClientRect();
+
+			if (!imageElement || !containerElement || !cropRect) {
 				return;
 			}
 
-			const { width: parentWidth, height: parentHeight } =
-				instance.wrapperComponent.getBoundingClientRect();
-			const { width, height } = cropRef.current.getBoundingClientRect();
+			const relativeImageRect = getRelativeBounds(containerElement, imageElement);
 
-			api.start({
-				x: x.get() + width > parentWidth ? parentWidth - width : x.get(),
-				y: y.get() + height > parentHeight ? parentHeight - height : y.get(),
-				immediate: true
-			});
+			const maxX = relativeImageRect.right - cropRect.width;
+			const maxY = relativeImageRect.bottom - cropRect.height;
+
+			const posX = clamp(x.get(), relativeImageRect.left, maxX);
+			const posY = clamp(y.get(), relativeImageRect.top, maxY);
+
+			api.start({ x: posX, y: posY, immediate: true });
 		}
 
 		window.addEventListener('resize', keepCropToolInBounds);
@@ -115,10 +144,11 @@ function CropTool() {
 		<animated.div
 			{...bind()}
 			ref={cropRef}
-			style={{ x, y }}
-			className="absolute left-2 top-2 z-10 h-32 w-32 origin-top-left cursor-move touch-none bg-red-500 text-xs text-white"
+			style={{ x: x.to(Math.round), y: y.to(Math.round) }}
+			className="panning-none absolute z-10 h-32 w-32 cursor-move touch-none bg-red-500 text-xs text-white"
+			// shadow-[0px_0px_0px_20000px_rgba(0,_0,_0,_0.50)]
 		>
-			<p className="select-none">{`x: ${x.get()} | y: ${y.get()} | s: ${scale}`}</p>
+			{/* <p className="select-none">{`x: ${x.get()} | y: ${y.get()} | s: ${scale}`}</p> */}
 		</animated.div>
 	);
 }
