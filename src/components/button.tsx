fce047ab7f7fc311c688730ad61d9267f9476516
jesseborg <6882782+jesseborg@users.noreply.github.com>
2023-11-28 17:36:47 +1000

    fix: button colors

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -12,7 +12,7 @@ const button = cva(
 	{
 		variants: {
 			variant: {
-				primary: ['text-white font-light', '[&:not(:disabled)]:hover:bg-neutral-800'],
+				primary: ['font-light', '[&:not(:disabled)]:hover:bg-neutral-800'],
 				secondary: [
 					'text-black bg-white border font-medium border-neutral-200',
 					'hover:bg-neutral-50'
@@ -20,7 +20,7 @@ const button = cva(
 				blank: ['bg-transparent']
 			},
 			disabled: {
-				true: 'cursor-not-allowed bg-neutral-200 !text-neutral-400'
+				true: 'cursor-not-allowed bg-neutral-200 text-neutral-400'
 			},
 			padding: {
 				normal: 'px-4 py-2',
@@ -34,7 +34,7 @@ const button = cva(
 			{
 				variant: 'primary',
 				disabled: false,
-				className: 'bg-neutral-950'
+				className: 'bg-neutral-950 text-white'
 			}
 		],
 		defaultVariants: {
